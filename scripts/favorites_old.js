2c8150dd006c542ffe2d87c6d2b119db7c89ad7b
Антон <anton.mail1902@yandex.ru>
2022-12-11 20:56:40 +0300

    final

--- scripts/favorites_old.js
+++ scripts/favorites_old.js
@@ -1,55 +1,19 @@
 'use strict'
 //скрипт добавления кнопки на статью в избранное
-var list, favorButton;
-const listBurger = document?.querySelector(".text__basket.burger");
-const favorButtonBurger = document?.querySelector(".basket.burger");
-
-const listNotBurger = document?.querySelector(".text__basket.not-burger");
-const favorButtonNotBurger = document?.querySelector(".basket.not-burger");
-
-
-function checkWidth(){
-    if (window.innerWidth < 870){
-        list = listBurger;
-        favorButton = favorButtonBurger;
-        refreshFavor();
-    }
-    else{
-        list = listNotBurger;
-        favorButton = favorButtonNotBurger;
-        refreshFavor();
-    }
-    favorButton?.click();
-}
-
-favorButtonBurger?.addEventListener("click", event => {
-    list = listBurger;
-    favorButton = favorButtonBurger;
-    refreshFavor();
-});
-
-favorButtonNotBurger?.addEventListener("click", event => {
-    list = listNotBurger;
-    favorButton = favorButtonNotBurger;
-    refreshFavor();
-});
-
+const list = document?.querySelector(".text__basket");
 const favors = document?.querySelectorAll(".add-to-favourites-btn, not-favor");
 const position = document?.querySelectorAll(".article-title");
-var counterBurger =document?.querySelector(".favourites-number.burger");
-var counterNotBurger =document?.querySelector(".favourites-number.not-burger");
-
+var counter = document?.querySelector(".favourites-number");
+const favorButton = document?.querySelector(".basket");
 var btnNames = [];
 
+favorButton.addEventListener("click", event => {
+    refreshFavor();
+});
+
 function refreshCounter(){
-    if (btnNames.length == 0) {
-        counterBurger.innerHTML ='';
-        counterNotBurger.innerHTML ='';
-    }
-    else {
-        counterBurger.innerHTML = Number(btnNames.length);
-        counterNotBurger.innerHTML = Number(btnNames.length);
-    }
+    if (btnNames.length == 0) counter.innerHTML ='';
+    else counter.innerHTML = Number(btnNames.length);
 }
 
 //обновление избранного
@@ -77,7 +41,6 @@ function refreshFavor(){
 
 for(let favor of favors){
     favor.addEventListener("click", event => {
-        console.log("favor clicked");
         //вычленяю кнопку "Далее" из статьи в которой вызвался event, 
         //ставлю ей текст названия статьи и добавляю в лист избранного
         let name = event.target.parentElement;
@@ -102,10 +65,7 @@ for(let favor of favors){
             }
         }
         //обновление избранного
-        checkWidth();
-        favorButton?.click();
-        favorButton?.click();
+        favorButton.click();
+        favorButton.click();
     });
 }
-
-checkWidth();
