5da60ab0de99463e31c3b9d3d41b1f13331c68e3
Антон <anton.mail1902@yandex.ru>
2022-12-12 09:37:05 +0300

    fixed favors script

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -7,7 +7,7 @@ const favorButtonBurger = document?.querySelector(".basket.burger");
 const listNotBurger = document?.querySelector(".text__basket.not-burger");
 const favorButtonNotBurger = document?.querySelector(".basket.not-burger");
 
-
+//проверка ширины экрана
 function checkWidth(){
     if (window.innerWidth < 870){
         list = listBurger;
@@ -42,6 +42,7 @@ var counterNotBurger =document?.querySelector(".favourites-number.not-burger");
 
 var btnNames = [];
 
+//обновление счетчика избранного
 function refreshCounter(){
     if (btnNames.length == 0) {
         counterBurger.innerHTML ='';
@@ -73,13 +74,32 @@ function refreshFavor(){
             text.append(btn)
             list.append(text);
         }
+        if (btnNames.length != 0){
+            let text = document.createElement("li");
+            var link = document.createElement('a');
+            link.appendChild(document.createTextNode("Очистить"));
+            link.setAttribute('href', "#");
+            if(link.addEventListener){
+                link.addEventListener('click', function(){
+                    clearLocalStorage();
+                });
+             }else if(link.attachEvent){
+                link.attachEvent('onclick', function(){
+                    clearLocalStorage();
+                });
+         }
+            text.append(link);
+            list.append(text);
+        }
     }
 }
 
+//функция загрузки данных в localstorage
 function save(){
-    var favorsSave = [];
+    let favorsSave = [];
     let namesSave = [];
-    
+    let namesSaveAbsLinks = [];
+
     for(let favor of favors){
         favorsSave.push(favor.classList.contains("favor"));
     }
@@ -88,19 +108,51 @@ function save(){
     for(let btn of btnNames){
         namesSave.push(btn.outerHTML.toString());
     }
+    for(let btn of btnsForSave){
+        namesSaveAbsLinks.push(btn.outerHTML.toString());
+    }
     localStorage.namesLS = JSON.stringify(namesSave);
+    localStorage.namesAbsLS = JSON.stringify(namesSaveAbsLinks);
 }
 
+//функция выгрузки данных из localstorage
 function load(){
     
     let favorsLoaded =  JSON.parse(localStorage.favorsLS);
     let btnLoaded = JSON.parse(localStorage.namesLS);
+    let btnAbsLoaded = JSON.parse( localStorage.namesAbsLS);
 
-    for(let btn of btnLoaded){
+    if (window.location.pathname.includes("index")){
+        for(let btn of btnLoaded){
+            let tempDiv = document.createElement('div');
+            if (btn.includes("div")){
+                btn = btn.replaceAll("<div>", '');
+                btn = btn.replaceAll("</div>", '');
+            }
+            tempDiv.innerHTML = btn;
+            btnNames.push(tempDiv);
+        }
+    }
+    else{
+        for(let btn of btnAbsLoaded){
+            let tempDiv = document.createElement('div');
+            if (btn.includes("div")){
+                btn = btn.replaceAll("<div>", '');
+                btn = btn.replaceAll("</div>", '');
+            }
+            tempDiv.innerHTML = btn;
+            tempDiv.innerHTML = btn;
+            btnNames.push(tempDiv);
+        }
+    }
+
+    for(let btn of btnAbsLoaded){
         let tempDiv = document.createElement('div');
         tempDiv.innerHTML = btn;
-        btnNames.push(tempDiv);
+        btnsForSave.push(tempDiv);
     }
+
+
     let i = 0;
     for(let favor of favors){
         if (!favorsLoaded[i]){
@@ -115,6 +167,15 @@ function load(){
     refreshCounter();
 }
 
+//очистка localstorage
+function clearLocalStorage(){
+    localStorage.favorsLS = [];
+    localStorage.namesLS = [];
+    localStorage.namesAbsLS = [];
+    location.reload();
+}
+
+var btnsForSave = [];
 for(let favor of favors){
     favor.addEventListener("click", event => {
         //вычленяю кнопку "Далее" из статьи в которой вызвался event, 
@@ -124,22 +185,33 @@ for(let favor of favors){
         name = name.parentElement.firstElementChild;
 
         let btn = document.createElement("button");
+        let tempBtn = document.createElement("button");
         btn.classList.add("read-article-btn-favor");
+        tempBtn.classList.add("read-article-btn-favor");
         btn.innerHTML = name.innerHTML;
-        btn.setAttribute("onclick", temp.getAttribute("onclick"));
+        tempBtn.innerHTML = name.innerHTML;
 
         let text = document.createElement("li");
         text.innerHTML = name.innerHTML;
+
+        let onclickText = temp.getAttribute("onclick");
+        onclickText = onclickText.substring(0, onclickText.indexOf("'")) + "'../" + onclickText.substring(onclickText.indexOf("'")+1);
+
         if(!event.target.classList.contains("not-favor")){
+            btn.setAttribute("onclick", temp.getAttribute("onclick"));
             btnNames.push(btn);
+            tempBtn.setAttribute("onclick", onclickText);
+            btnsForSave.push(tempBtn);
         }
         else {//если убираем из избранного
             for (let i = btnNames.length - 1; i >= 0; --i) {
                 if (btnNames[i].innerHTML.toString().includes(name.innerHTML)) {
                     btnNames.splice(i,1);
+                    btnsForSave.splice(i,1);
                 }
             }
         }
+        //загрузка данных в localstorage
         save();
         //обновление избранного
         checkWidth();
@@ -149,3 +221,4 @@ for(let favor of favors){
 }
 checkWidth();
 load();
+//localStorage.clear();
